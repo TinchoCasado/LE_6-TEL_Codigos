2ecfc19fda36e216b879a29b3a3517e84a40688e
Martin Casado <132573375+TinchoCasado@users.noreply.github.com>
2023-08-31 11:31:19 -0300

    Update Mef_Antirrebote_Conteo_V2.0.ino

--- LE_MefAntirrebotes_Conteo_V2.0_6to/Mef_Antirrebote_Conteo_V2.0.ino
+++ LE_MefAntirrebotes_Conteo_V2.0_6to/Mef_Antirrebote_Conteo_V2.0.ino
@@ -25,9 +25,9 @@ const char pepito[16]
 #define LED1_READ 13
 #define LED2_READ 11
 #define DEMORA 40
+#define VAL_MAX 15
 
-int d=0;
-bool flag;
+bool botonPres=false;
 unsigned long t_now;
 typedef enum
 {
@@ -41,7 +41,7 @@ void debounceFSM_init();   //ejecutada una sola vez
 void debounceFSM_update(); //se ejecuta siempre
 void buttonPressed();      //se ejecuta cuando estado==button_up
 void buttonReleased();     //se ejecuta cuando estado==button_down
-int print(int);          //se ejecuta al presionar el botón para imprimir en el display
+int print(int, char);          //se ejecuta al presionar el botón para imprimir en el display
 debounceState_t estado;
 
 void setup ()
@@ -56,17 +56,21 @@ void setup ()
   //Serial.begin(9600);       // Se inicializa el puerto serie
   debounceFSM_init();       // Se ejecuta una sola vez la función "init"
   t_now = millis();
+  print(0); //El display cuenta cada vez que el botón es presionado
+
 }
 
 void loop()
 {
+  static int cuenta=1;
   debounceFSM_update();
   //Serial.println(digitalRead(PULSADOR)); chequeo de tensión del pulsador
-  if (flag==1)
+  if (botonPres)
   {
-    d = print(d); //El display cuenta cada vez que el botón es presionado
-    flag = 0;
+    cuenta=print(cuenta); //El display cuenta cada vez que el botón es presionado
+    botonPres = false;
   }
+  //PORTD = 0b00110011;
 }
 
 void debounceFSM_init() //Establece a "estado" en 0 (BUTTON_UP)
@@ -80,12 +84,12 @@ void debounceFSM_update()
   {
   case BUTTON_UP:
     t_now = millis();
-    Serial.println("estado: BUTTON_UP");
+    //Serial.println("estado: BUTTON_UP");
     if (digitalRead(PULSADOR)==0) estado = BUTTON_FALLING;
     break;
     
   case BUTTON_FALLING:
-    Serial.println("estado: BUTTON_FALLING");
+    //Serial.println("estado: BUTTON_FALLING");
     if (millis() - t_now > DEMORA)
     {
        if (digitalRead(PULSADOR)==0)
@@ -99,12 +103,12 @@ void debounceFSM_update()
     
   case BUTTON_DOWN:
     t_now = millis();
-    Serial.println("estado: BUTTON_DOWN");
+    //Serial.println("estado: BUTTON_DOWN");
     if (digitalRead(PULSADOR)==1) estado = BUTTON_RISING;
     break;
     
   case BUTTON_RISING:
-    Serial.println("estado: BUTTON_RISING");
+    //Serial.println("estado: BUTTON_RISING");
     if (millis() - t_now > DEMORA)
     {
        if (digitalRead(PULSADOR)==1)
@@ -122,7 +126,7 @@ void buttonPressed() //Invierto el estado del led 1
 {
   if (digitalRead(LED1_READ)==0) digitalWrite(LED1_OUT, HIGH);
   else digitalWrite(LED1_OUT, LOW);
-  flag = 1;
+  botonPres = true;
 }
 
 void buttonReleased() //Invierto el estado del led 2
@@ -131,8 +135,9 @@ void buttonReleased() //Invierto el estado del led 2
   else digitalWrite(LED2_OUT, LOW);
 }
 
-int print(int d){    //Función para imprimir los dígitos
-  PORTD = pepito[d];
-  if (d<15) d++;
-  else d=0;
+int print(int valor)    //Función para imprimir los dígitos
+{
+  PORTD = pepito[valor++];
+  if (valor>VAL_MAX)valor=0;
+  return valor;
 }
